1a63e5b9c9a67f85196df6a67a73600808b91b59
vspath800 <krackermal441@gmail.com>
2024-08-16 07:25:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest=function(request,response){
 	response.writeHead(200);
-	response.end("<h1> Welcome to cloud build using CICD GKE first run manual</h1>");
+	response.end("<h1> Welcome to cloud build using CICD GKE second run manual</h1>");
 }
 var www=http.createServer(handleRequest);
 www.listen(8080);
